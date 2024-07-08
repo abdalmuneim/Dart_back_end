5c753c142e63b24069cbb3f18fce52d74216a3d5
Alaa Al-Daour <alaa.daour98@gmail.com>
2024-07-09 00:59:54 +0300

    1st commit

--- lib/tables.dart
+++ lib/tables.dart
@@ -4,6 +4,6 @@ import 'package:drift/drift.dart';
 class ExampleTable extends Table {
   IntColumn get id => integer().autoIncrement()();
   TextColumn get name => text().withLength(min: 1, max: 50)();
-  TextColumn get desc => text().withLength(min: 1, max: 50)();
+  TextColumn get desc => text().withLength(min: 1, max: 500).nullable()();
 
 }
